c962be1acbaeaa5c40ab67256d30fdef20b06921
hs414171 <hs414171@gmail.com>
2021-10-29 16:21:22 +0530

    re configed

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const PORT = 8000
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/data', (req, res)=>{
     res.send("helllllllllllooooooooooooooooooooo")
 })
 
 app.post('/post', (req, res)=>{
+    console.log(req.body)
     res.send(req.body.name)
 })
 
