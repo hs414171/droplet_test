77027aa88f6404d2850f3552d73c44ea6e460131
hs414171 <hs414171@gmail.com>
2021-10-29 15:58:45 +0530

    init server

---
+++ server.js
@@ -0,0 +1,11 @@
+const PORT = 8000
+const express = require('express')
+const app = express()
+
+app.get('/data', (req, res)=>{
+    res.send("helllllllllllooooooooooooooooooooo")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started at port ${port}`)
+})
